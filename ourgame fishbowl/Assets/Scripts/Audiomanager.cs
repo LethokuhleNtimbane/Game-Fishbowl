c83337459dec0c0ff0d478303c4b4809a23f7177
LethokuhleNtimbane <2988445@students.wits.ac.za>
2026-04-17 22:03:59 +0200

    Main Menu almost done

---
+++ ourgame fishbowl/Assets/Scripts/Audiomanager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Audiomanager : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] Slider slider;
+    //[SerializeField] AudioSource SFX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public AudioClip background;
+    //public AudioClip soundeffects;
+    void Start()
+    {
+        audioSource.clip = background;
+        audioSource.Play();
+
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            load();
+
+        }
+        else
+        {
+            load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = slider.value;
+        save();
+    }
+
+    private void load()
+    {
+       slider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", slider.value);
+    }
+
+   
+
+}
