1dcf0d77b5a656a4ab9e348d800426f659e8e56a
LethokuhleNtimbane <2988445@students.wits.ac.za>
2026-04-19 09:40:28 +0200

    Game almost done

--- ourgame fishbowl/Assets/Scripts/Objects move/SlidingDoor.cs
+++ ourgame fishbowl/Assets/Scripts/Objects move/SlidingDoor.cs
@@ -1,29 +1,86 @@
-using UnityEngine;
 using System.Collections;
+using TMPro;
+using UnityEngine;
 
 public class SlidingDoor : MonoBehaviour
 {
     [SerializeField] Animator animator;
-    private bool toplay = true;
-    public GameObject sawyou;
-    public float speedopen;
+    public SpriteRenderer JordgeJnr;
+    public Sprite Jordge;
+   
 
+    public float speedopen = 2f;
+
+    private bool doorOpen = false;
+    private bool Playerin = false;
+
+    public GameObject youlost;
+    public GameObject disapear;
+    public GameObject exitbutton;
+    public GameObject pausemenu;
+    public Movement movement;
+    public TextMeshProUGUI howlost;
     private void Start()
     {
-        StartCoroutine(LoopAnimations());
+        animator.SetTrigger("Play");
     }
 
-    IEnumerator LoopAnimations()
+    public void DoorOpened()
     {
-        while (toplay == true)
-        {
-            yield return new WaitForSeconds(speedopen);
-            animator.Play("Sliding door animation");
+        doorOpen = true;
+    }   
 
+    public void DoorClosed()
+    {
+        doorOpen = false;
+        StartCoroutine(Door());
+    }
+
+    IEnumerator Door()
+    {
+        
             yield return new WaitForSeconds(speedopen);
-            animator.Play("Sliding door animation 0");
+            animator.SetTrigger("Play");
+
+            
+    }
+
+    private void Update()
+    {
+        if (Playerin && doorOpen == true)
+        {
+            float upDown = Input.GetAxis("Vertical");
+            float sideways = Input.GetAxis("Horizontal");
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+            {
+                youlost.SetActive(true);
+                pausemenu.SetActive(true);
+                howlost.text = "You got caught by Jordge Jnr";
+                JordgeJnr.sprite = Jordge;
+                disapear.SetActive(false);
+                exitbutton.SetActive(false);
+                movement.enabled = false;
+                Time.timeScale = 0f;
+            }
         }
     }
 
-    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+                Playerin = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Playerin = false;
+        }
+    }
+
+
 }
